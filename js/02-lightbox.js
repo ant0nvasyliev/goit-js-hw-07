f0b704b4563c2467398a009174f93025fafd292b
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-06-10 09:47:44 +0100

    hm-07/attempt-01

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+if (galleryContainer) {
+  galleryContainer.addEventListener("click", onGalleryContainerClick);
+}
+
+function onGalleryContainerClick(event) {
+  if (event.target.classList.contains("gallery__image")) {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captions: true,
+      captionDelay: 250,
+    });
+  }
+}
